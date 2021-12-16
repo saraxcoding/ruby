11c7365e449f78d539b832d253e1675dbe18ab5a
sara_h <smoke_screen16@hotmail.com>
2021-12-16 01:27:21 -0500

    Added favorite things array

--- arrayhash.rb
+++ arrayhash.rb
@@ -1,3 +1,10 @@
 puts fav_foods = ['mashed potatoes', 'spicy fried chicken', 'cheese danish']
 fav_foods.count
 most_fav_food = fav_foods.fetch(0)
+fav_things = ['parents', 'cats', 'novels']
+fav_things.push(most_fav_food)
+fav_things = fav_things.sort
+fav_things.delete('novels')
+fav_things_str = fav_things.join(', ')
+puts "My favorite things EVER, in alphabetical order, are: #{fav_things_str}."
+fav_books_movies = {"books" => ["His Dark Materials", ""], "movies" => ["", ""]}
