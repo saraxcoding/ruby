36a5a83f42b6f0860d1c71a59a8100283babab92
sara_h <smoke_screen16@hotmail.com>
2021-12-18 23:13:46 -0500

    Added a condition for chosen weather

--- case.rb
+++ case.rb
@@ -24,11 +24,13 @@ current_weather = 'foggy'
 case current_weather
 when 'sunny'
     puts "Wear short sleeves, shorts and sunglasses!"
+when 'foggy'
+    puts "Have a jacket on you, just in case!"
 when 'snowy'
     puts "Puffer jackets with snow boots, for sure!"
 when 'rainy'
     puts "Wear a raincoat and rain boots, and don't forget an umbrella!"
 when 'chilly'
     puts "Perfect time to don that favorite ugly sweater!"
-else puts "Hmm, I don't know what you should wear in that kind of weather! Have a jacket on you, just in case!"
+else puts "Hmm, I don't know what you should wear in that kind of weather!"
 end
