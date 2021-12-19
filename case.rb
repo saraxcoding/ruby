c4c9d9ab33c22eb19a6bda5a4e441d83f084ce3f
sara_h <smoke_screen16@hotmail.com>
2021-12-18 23:11:58 -0500

    Used case statement for weather dressing advice

--- case.rb
+++ case.rb
@@ -16,3 +16,19 @@ when 'purple'
 else 
     puts "Hmm, well I don't know what that color is!"
 end
+
+#weather case statement starts here:
+
+current_weather = 'foggy'
+
+case current_weather
+when 'sunny'
+    puts "Wear short sleeves, shorts and sunglasses!"
+when 'snowy'
+    puts "Puffer jackets with snow boots, for sure!"
+when 'rainy'
+    puts "Wear a raincoat and rain boots, and don't forget an umbrella!"
+when 'chilly'
+    puts "Perfect time to don that favorite ugly sweater!"
+else puts "Hmm, I don't know what you should wear in that kind of weather! Have a jacket on you, just in case!"
+end
