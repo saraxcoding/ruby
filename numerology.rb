86ddcc40aba93b08fb536a3432c37dc4cfc778ac
sara_h <smoke_screen16@hotmail.com>
2021-12-19 23:11:07 -0500

    Included first addition portion of exercise

---
+++ numerology.rb
@@ -0,0 +1,13 @@
+# First, ask the user for their birthdate. It must be in the format MMDDYYYY
+puts "What is your birthdate (MMDDYYYY)?"
+# Use the gets method to get their birthdate & assign it to a variable
+birthdate = gets
+# Add all the numbers of their birthdate together using array syntax & assign the result to a new variable called number
+number = birthdate[0].to_i +
+birthdate[1].to_i + 
+birthdate[2.to_i + 
+birthdate[3].to_i +
+birthdate[4].to_i +
+birthdate[5].to_i +
+birthdate[6].to_i +
+birthdate[7].to_i
