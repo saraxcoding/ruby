6a0223ef6500a3e36abae4e99d69faaf89b2dafc
sara_h <smoke_screen16@hotmail.com>
2021-12-22 23:50:10 -0500

    Fixed typos

--- my_first_method.rb
+++ my_first_method.rb
@@ -1,10 +1,10 @@
-def "my_first_method"
+def my_first_method
     puts "This is my first method!"
 end
 
 my_first_method
 
-def "two_two_method"
+def two_two_method
     number = 2 + 2
     puts number
 end
