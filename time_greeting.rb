eba497f30371fb0329d123575edc5d62665e668c
sara_h <smoke_screen16@hotmail.com>
2021-12-25 00:48:29 -0500

    Fixed time_greeting.rb

--- time_greeting.rb
+++ time_greeting.rb
@@ -3,27 +3,18 @@ def determine_current_hour
     current_time.hour
 end
 
-def greeting(name)
+def greeting
     current_hour = determine_current_hour
-    if(current_hour > 3 && current_hour < 12)
-        time = "morning"
-    elsif(current_hour > 12 && current_hour < 18)
-        time = "afternoon"
-    elsif(current_hour > 18 || current_hour < 2)
-        time = "evening"
-    end
-
-    return current_hour
+    puts "What is your name?"
+    name = gets
+        if current_hour > 3 && current_hour < 12
+            time = "morning"
+        elsif current_hour > 12 && current_hour < 18
+            time = "afternoon"
+        elsif current_hour > 18 || current_hour < 2
+            time = "evening"
+        end
+    puts "Good #{time}, #{name.capitalize}!"
 end
 
-def personalized_greeting(name)
-    current_name = greeting(name)
-    if(current_name === "Emily")
-        puts "Good #{time}, #{name.capitalize}!"
-    else 
-        puts "Oh, I don't know who you are, but greetings!"
-    end
-end 
-
-personalized_greeting("Sara")
-personalized_greeting("Emily")
+greeting
