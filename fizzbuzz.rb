0463c49325d63d5fd3f1794a52f4f7404d376fc1
sara_h <smoke_screen16@hotmail.com>
2021-12-26 19:28:17 -0500

    Added conditionals

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+# numbers 1 to 100
+# three conditionals
+
+i = 0
+while i < 101
+    if i%3 == 0 && i%5 == 0 # number || multiple of three and five "FizzBuzz"
+        puts "Fizzbuzz"
+    elsif i%3 == 0 # number = multiple of three "Fizz"
+        puts "Fizz"
+    elsif i%5 == 0 # number = multiple of five "Buzz"
+        puts "Buzz"
+    else 
+        puts i
+    end
+    i+=1 # displays the numbers from 1 to 100 sequentially 
+end
