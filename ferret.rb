0e7dc98c0c6a70084ad32ab67e3e9a0912bdac3b
sara_h <smoke_screen16@hotmail.com>
2021-12-27 23:52:30 -0500

    Added comments and fixed typos

--- ferret.rb
+++ ferret.rb
@@ -1,5 +1,5 @@
-class Ferret
-    def set_name = (ferret_name)
+class Ferret # define an object class
+    def set_name = (ferret_name) # define a method
         @name = ferret_name
     end
     
@@ -14,3 +14,12 @@ class Ferret
     def get_owner
         return @owner_name
     end
+
+    def squeal
+        return "squeeee"
+    end
+end
+
+my_ferret = Ferret.newmy_ferret.set_name = "Fredo"
+ferretname = my_ferret.get_name
+puts "#{ferretname} says #{ferret_squeal}"
