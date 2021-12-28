b3497297f64a91099e072dfcbe34ade9a959e707
sara_h <smoke_screen16@hotmail.com>
2021-12-27 23:48:52 -0500

    Added objects

---
+++ ferret.rb
@@ -0,0 +1,16 @@
+class Ferret
+    def set_name = (ferret_name)
+        @name = ferret_name
+    end
+    
+    def get_name
+        return @name
+    end
+
+    def set_owner = (owner_name)
+        @owner_name = owner_name
+    end
+
+    def get_owner
+        return @owner_name
+    end
