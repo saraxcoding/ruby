8bb973f9455699492ab03f9ad9e8d73fb3842985
sara_h <smoke_screen16@hotmail.com>
2021-12-28 23:01:05 -0500

    Initial commit

---
+++ debugging_pets.rb
@@ -0,0 +1,93 @@
+class Ferret # need to capitalize class name
+ 
+	def set_name= (ferret_name) # remove spacing between = sign
+		@name = ferret_name
+	end
+ 
+	def get_name
+		return @name
+	end
+ 
+	def set_owner=(owner_name)
+		@owner_name = owner_name
+	end
+ 
+	def get_owner
+		return @owner_name
+	end
+ 
+	def squeal
+		return "squeeeeee"
+	end
+ 
+end
+ 
+class Chinchilla # misspelled chinchilla
+ 
+	def set_name=(chinchilla_name)
+		@name = chinchilla_name
+	end
+
+    # missing get_name method
+    def get_name
+        return @name
+    end
+ 
+	def set_owner=(owner_name)
+		@owner_name = owner_name
+	end
+ 
+	def get_owner
+		return @owner_name
+	end
+ 
+	def squeek
+		return "eeeep"
+	end
+ 
+end
+ 
+class Parrot
+ 
+	def set_name=(parrot_name)
+		@name = parrot_name # fixed
+	end
+ 
+	def get_name
+		return @name
+	end
+ 
+	def set_owner=(owner_name)
+		@owner_name = owner_name
+	end
+ 
+	def get_owner
+		return @owner_name
+	end
+ 
+    # missing tweet method
+    def tweet
+        return "tweet"
+    end
+
+end
+ 
+my_ferret = Ferret.new
+my_ferret.set_name= "Fredo"
+ferretname = my_ferret.get_name
+ 
+my_parrot = Parrot.new
+my_parrot.set_name= "Budgie"
+parrotname = my_parrot.get_name
+ 
+my_chincilla = Chincilla.new
+my_chincilla.set_name= "Dali"
+chincillaname = my_chincilla.get_name
+ 
+puts "#{ferretname} says #{my_ferret.squeal}, 
+#{parrotname} says #{my_parrot.tweet}, 
+and #{chincillaname} says #{my_chincilla.squeek}."
+ 
+puts my_ferret.inspect
+puts my_parrot.inspect
+puts my_chincilla.inspect
