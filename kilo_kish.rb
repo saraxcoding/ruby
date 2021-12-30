3c20cc03ec6c8edf4a5cefa13055899b7c9a6027
sara_h <smoke_screen16@hotmail.com>
2021-12-29 23:55:32 -0500

    Fixed typos

--- kilo_kish.rb
+++ kilo_kish.rb
@@ -41,5 +41,22 @@ studiodebut = studio_debut.get_song
 studio_debut.set_album= "Reflections in Real Time"
 studiodebutalbum = studio_debut.get_album
 
-puts "#{studiodebut} is from #{studiodebutalbum}, released on #{studio_debut.album_one}"
+second_EP = Elegance.new
+second_EP.set_song= "Elegance"
+secondEP = second_EP.get_song
+second_EP.set_album= "mothe"
+secondEPalbum = second_EP.get_album
 
+third_EP = NICE_OUT.new
+third_EP.set_song= "NICE OUT"
+thirdEP = third_EP.get_song
+third_EP.set_album= "REDUX"
+thirdEPalbum = third_EP.get_album
+
+puts "#{studiodebut} is from #{studiodebutalbum}, released on #{studio_debut.album_one};
+#{secondEP} is from #{secondEPalbum}, released on #{second_EP.album_two}; 
+#{thirdEP} is from #{thirdEPalbum}, released on #{third_EP.album_three}"
+
+puts studio_debut.inspect
+puts second_EP.inspect
+puts third_EP.inspect
