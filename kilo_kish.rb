00f4334a3b707a3e000b4d34ff3151815f6ee6f2
sara_h <smoke_screen16@hotmail.com>
2021-12-29 23:21:41 -0500

    Created new ruby file

---
+++ kilo_kish.rb
@@ -0,0 +1,23 @@
+class Kilo_Kish
+    def set_song= (song)
+        @name set_song
+    end
+
+    def get_song
+        return @name
+    end
+
+    def set_album= (album)
+        @album_name = album_name
+    end
+
+    def get_album
+        return @album_name
+    end
+
+end
+
+
+
+puts "#{name} is from #{}, released in #{}, "
+
