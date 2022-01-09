07b80a4d88bb2fd2da4326d1499b2c68639a789f
sara_h <smoke_screen16@hotmail.com>
2022-01-08 23:53:18 -0500

    Edited methods

--- blog.rb
+++ blog.rb
@@ -1,54 +1,85 @@
 class Blog
 
-    puts "Do you want to create another blog post? [Y/N]"
-    answer = gets.chomp.downcase
-
     while (answer.downcase == "y")
         ""
     end
 
-    @all_blog_posts = []
+    @@all_blog_posts = []
     @@total_blog_posts = 0 
 
-    def initialize
+    def self.all
         @@all_blog_posts
+    end
+
+    def self.add(thing)
+        @@all_blog_posts << thing
+        @@total_blog_posts += 1
     end
 
     def self.publish
         @all_blog_posts.each do |post|
-            puts "Title: \n #{post.title}"
+            puts "Title:\n #{post.title}"
+            puts "Body:\n #{post.content}"
+            puts "Publish Date:\n #{post.created_at}"
+            puts "Author:\n #{post.author}"
+        end
+    end
+end
+
+
+class BlogPost < Blog
+
+    def self.create
+        post = new
+        puts "Name your blog post:"
+        post.title = gets.chomp
+        puts "Your blog post content:"
+        post.content = gets.chomp
+        post.created_at = Time.now
+        post.author = gets.chomp
+        post.save
+        puts "Do you want to create another blog post? [Y/N]"
+        create if gets.chomp.downcase == 'y'
     end
 
-
-    def set_title=(title)
+    def title
         @title = title
     end
 
-    def get_title
-        return @title
+    def title=(title)
+        @title = title 
     end
 
-    define set_content=(content)
+    def content
         @content = content
     end
 
-    def get_content
-        return @content
+    def content=(content)
+        @content = content
     end
 
-    def set_publish_date=(publish_date)
-        @publish_date = publish_date
+    def created_at
+        @created_at
     end
 
-    def get_publish_date
-        return @publish_date
+    def created_at=(created_at)
+        @created_at = created_at
     end
     
-    define set_author=(author)
+    def author=(author)
         @author = author
     end
 
-    def get_author
-        return @author
+    def author
+        @author = author
+    end
+
+    def save
+        BlogPost.add(self)
     end
 end
+
+BlogPost.create
+all_blog_posts = BlogPost.all
+puts all_blog_posts.inspect
+BlogPost.publish
