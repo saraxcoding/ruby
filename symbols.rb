998f278aae81ce306702477e4c7ab8ac1a328d5f
sara_h <smoke_screen16@hotmail.com>
2022-01-18 22:52:48 -0500

    Added new ruby file

---
+++ symbols.rb
@@ -0,0 +1,16 @@
+my_hash = {
+    "key1" => "value1", 
+    "key2" => "value2"
+}
+
+entry_01 = { :name => "Kelly", :email => "kelly.wonderful@gmail.com"}
+entry_02 = { :name => "Kat", :email => "kate.wonderful@gmail.com"}
+entry_03 = { :name => "Tom", :email => "tom.wonderful@gmail.com"}
+entry_04 = { :name => "Timothy", :email => "timothy.wonderful@gmail.com"}
+
+winnder = entry_03[:name]
+
+puts "#{winner} is the winner of the Mac laptop!"
+
+# can rewrite the hashes as so
+# entry_01 = { name: "Kelly", email: "kelly.wonderful@gmail.com"}
