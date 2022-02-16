01a6d29b420e6c770f082d9bb3406b9e6daecc3f
sara_h <smoke_screen16@hotmail.com>
2022-02-15 20:51:29 -0500

    Fixed typos

--- mitski_hash.rb
+++ mitski_hash.rb
@@ -1,6 +1,41 @@
-lush = {:name => "Liquid Smooth", :name => "Pearl Diver"}
-rfsncb = {:name => "Goodbye, My Danish Sweetheart", :name => "I Want You"}
-bmamc = {:name => "I Will", :name => "First Love/Late Spring"}
-puberty_2 = {:name => "Once More to See You", :name => "Your Best American Girl", :name => "I Bet on Losing Dogs", :name => "Thursday Girl", :name => "Crack Baby"}
-btc = {:name => "Me and My Husband", :name => "A Pearl", :name => "Washing Machine Heart", :name => "Two Slow Dancers"}
-laurel_hell = {:name => "Valentine, Texas", :name=> "Working for the Knife", :name => "Stay Soft", :name => "Heat Lightning", :name => "I Guess", :name => "Should've Been Me"}
+lush = {
+    :song1 => "Liquid Smooth", 
+    :song2 => "Pearl Diver"
+}
+
+rfsncb = {
+    :song1 => "Goodbye, My Danish Sweetheart", 
+    :song2 => "I Want You"
+}
+
+bmamc = {
+    :song1 => "I Will", 
+    :song2 => "First Love/Late Spring"
+}
+
+puberty_2 = {
+    :song1 => "Once More to See You", 
+    :song2 => "Your Best American Girl", 
+    :song3 => "I Bet on Losing Dogs", 
+    :song4 => "Thursday Girl", 
+    :song5 => "Crack Baby"}
+
+btc = {
+    :song1 => "Me and My Husband", 
+    :song2 => "A Pearl", 
+    :song3 => "Washing Machine Heart", 
+    :song4 => "Two Slow Dancers"
+}
+
+laurel_hell = {
+    :song1 => "Valentine, Texas", 
+    :song2 => "Working for the Knife", 
+    :song3 => "Stay Soft", 
+    :song4 => "Heat Lightning", 
+    :song5 => "I Guess", 
+    :song6 => "Should've Been Me"
+}
+
+favorite = laurel_hell[:song3]
+
+puts "{#favorite} is my current favorite song by Mitski!"
