23c87f7a3d54abe8930a97371aa29d609c1e84e9
sara_h <smoke_screen16@hotmail.com>
2022-02-15 20:44:17 -0500

    Added new file

---
+++ mitski_hash.rb
@@ -0,0 +1,6 @@
+lush = {:name => "Liquid Smooth", :name => "Pearl Diver"}
+rfsncb = {:name => "Goodbye, My Danish Sweetheart", :name => "I Want You"}
+bmamc = {:name => "I Will", :name => "First Love/Late Spring"}
+puberty_2 = {:name => "Once More to See You", :name => "Your Best American Girl", :name => "I Bet on Losing Dogs", :name => "Thursday Girl", :name => "Crack Baby"}
+btc = {:name => "Me and My Husband", :name => "A Pearl", :name => "Washing Machine Heart", :name => "Two Slow Dancers"}
+laurel_hell = {:name => "Valentine, Texas", :name=> "Working for the Knife", :name => "Stay Soft", :name => "Heat Lightning", :name => "I Guess", :name => "Should've Been Me"}
